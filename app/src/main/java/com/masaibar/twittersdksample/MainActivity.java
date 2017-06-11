04304aa19ac10c59654658d715bfcb2567def45d
masaibar <orebatemoko@gmail.com>
2017-06-11 22:22:19 +0900

    Login and get userData.

--- app/src/main/java/com/masaibar/twittersdksample/MainActivity.java
+++ app/src/main/java/com/masaibar/twittersdksample/MainActivity.java
@@ -1,13 +1,50 @@
 package com.masaibar.twittersdksample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TwitterLoginButton mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        mLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        mLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session = result.data;
+                long userId = session.getUserId();
+                String userName = session.getUserName();
+
+                Log.d("!!!", String.format("userId = %s, userName = %s", userId, userName));
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("!!!", String.format("failure, %s", exception.getMessage()));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mLoginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
