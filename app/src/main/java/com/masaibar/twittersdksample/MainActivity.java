5cdf801bdad62f772e71f0d9b3a636cf653f9728
masaibar <orebatemoko@gmail.com>
2017-06-12 00:02:52 +0900

    Tweet from app.

--- app/src/main/java/com/masaibar/twittersdksample/MainActivity.java
+++ app/src/main/java/com/masaibar/twittersdksample/MainActivity.java
@@ -1,15 +1,17 @@
 package com.masaibar.twittersdksample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.Tweet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupViews() {
+        setupLoginButton();
+        setupTweetButton();
+    }
+
+    private void setupLoginButton() {
         mLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
         mLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
@@ -41,6 +48,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setupTweetButton() {
+        findViewById(R.id.button_tweet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TwitterWrapper twitterWrapper = new TwitterWrapper(getApplicationContext());
+                twitterWrapper.tweet("test tweet", new Callback<Tweet>() {
+                    @Override
+                    public void success(Result<Tweet> result) {
+                        Log.d("!!!", "Tweet succeeded");
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        Log.d("!!!", String.format("Tweet failed, %s", exception.getMessage()));
+                    }
+                });
+            }
+        });
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/masaibar/twittersdksample/TwitterWrapper.java
+++ app/src/main/java/com/masaibar/twittersdksample/TwitterWrapper.java
@@ -3,10 +3,15 @@ package com.masaibar.twittersdksample;
 import android.content.Context;
 import android.util.Log;
 
+import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.models.Tweet;
 
 public class TwitterWrapper {
 
@@ -26,5 +31,19 @@ public class TwitterWrapper {
                 .build();
 
         Twitter.initialize(config);
+    }
+
+    public void tweet(String text, Callback<Tweet> callback) {
+        getApiClient().getStatusesService()
+                .update(text, null, null, null, null, null, null, null, null)
+                .enqueue(callback);
+    }
+
+    public TwitterSession getSession() {
+        return TwitterCore.getInstance().getSessionManager().getActiveSession();
+    }
+
+    public TwitterApiClient getApiClient() {
+        return TwitterCore.getInstance().getApiClient(getSession());
     }
 }
